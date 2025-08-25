515d5725dbdf2144209781f56d316a58305c41a8
Esmaeeil Enani <esmaeeilenani@gmail.com>
2025-08-25 08:25:47 +0300

    - log correlation-id inside CustomerController

--- accounts/src/main/java/com/esmaeeil/eazybank/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/esmaeeil/eazybank/accounts/controller/CustomerController.java
@@ -1,18 +1,16 @@
 package com.esmaeeil.eazybank.accounts.controller;
 
 
-import com.esmaeeil.eazybank.accounts.dto.CustomerDto;
 import com.esmaeeil.eazybank.accounts.dto.CustomerFullDetailsDto;
 import com.esmaeeil.eazybank.accounts.service.CustomerService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/customers")
@@ -25,8 +23,11 @@ public class CustomerController {
     public ResponseEntity<CustomerFullDetailsDto> getCustomer(
             @Valid @RequestParam
             @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
-            String mobileNumber
+            String mobileNumber,
+            @RequestHeader(name = "eazybank-correlation-id", required = false) String correlationId
     ) {
+        log.debug("eazybank-correlation-id found: {}", correlationId);
+
         return ResponseEntity.ok(customerService.getCustomerFullDetailsByMobileNumber(mobileNumber));
     }
 
